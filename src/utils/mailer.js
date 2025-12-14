cddce564fcd4978f688ad2e25b30e191fc9d66cb
Nguyen Van Huong <147009769+NguyenHuong90@users.noreply.github.com>
2025-12-14 12:25:14 +0700

    Implement email sending functionality with nodemailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,1 +1,27 @@
+const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // App Password
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  try {
+    await transporter.sendMail({
+      from: `"System" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log(" Email sent to:", to);
+  } catch (error) {
+    console.error(" Send email error:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
